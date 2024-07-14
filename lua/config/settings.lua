b37c6a4fa2f10adebad37a04a6b1d6bca40fed0b
Joao Pedro Veloso <JPV@ly9.lymba.com>
2024-07-13 21:39:47 -0500

    adding clipboard settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,3 +1,21 @@
 -- Undotree directory for containerized environment
 vim.opt.undodir = "/workspace/.vim-undo"
 vim.opt.undofile = true
+-- Use the "+" register on Neovim for general clipboard
+vim.o.clipboard = "unnamedplus"
+-- Select system clipboard depending on Mac/Linux
+if vim.fn.has('unix') == 1 then
+  vim.g.clipboard = {
+    name = 'xclip',
+    copy = {
+      ['+'] = 'xclip -selection clipboard',
+      ['*'] = 'xclip -selection primary',
+    },
+    paste = {
+      ['+'] = 'xclip -selection clipboard -o',
+      ['*'] = 'xclip -selection primary -o',
+    },
+    cache_enabled = 0,
+  }
+end
+
